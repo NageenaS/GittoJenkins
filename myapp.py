a58155ec37319f80eeefa4ccd4f1cce4156c8ccb
ShaikNageena <nageena.official@gmail.com>
2024-10-04 12:42:55 +0530

    myapp.py

--- myapp.py
+++ myapp.py
@@ -12,11 +12,11 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello World'
+    return 'Hello World!'
 
 # main driver function
 if __name__ == '__main__':
 
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=6000)
